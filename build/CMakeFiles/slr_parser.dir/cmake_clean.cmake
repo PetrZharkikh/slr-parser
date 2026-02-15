05238b4b87110892e97ce531325e8fe751931d7c
Петр Жарких <petrzarkih@MacBook-Air-Petr.local>
2026-02-16 02:00:10 +0300

    Add minimal cmake and main

---
+++ build/CMakeFiles/slr_parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slr_parser.dir/src/main.cpp.o"
+  "CMakeFiles/slr_parser.dir/src/main.cpp.o.d"
+  "slr_parser"
+  "slr_parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slr_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/slr_parser.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for slr_parser.
+# This may be replaced when dependencies are built.
